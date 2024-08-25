c930e265d07c3e545317f7e95d64be9e376d2f6f
james14153 <jamesvincent903@gmail.com>
2024-08-25 14:01:54 -0700

    corecting

--- p_print.c
+++ p_print.c
@@ -14,29 +14,39 @@ int p_print(va_list arguments)
 	char array[50];
 	unsigned long number;
 	char *hx = "0123456789abcdef";
-	int a = 0; 
+	int a = 2; 
 	int b, c = 0;
 
 	if (!ptr)
 	{
-		return (write(1, "(Null)", 6));
+		return (write(1, "(null)", 6));
 	}
 
 	number = (unsigned long)ptr;
-	array[a++] = 'x';
-	array[a++] = '0';
-
-	while (number > 0)
+	array[0] = '0';
+	array[1] = 'x';
+	
+	if (number == 0)
 	{
-		array[a++] = hx[number % 16];
-		number /= 16;
+		array[a++] = '0';
 	}
-
-	for (b = a - 1; b >= 0; b--)
+	else
 	{
-		write(1, &array[b], 1);
-		c++;
+
+		while (number > 0)
+		{
+			array[a++] = hx[number % 16];
+			number /= 16;
+		}
+
+	for (b = 2; end = a - 1; b++; end--)
+	{
+		char temp = array[b];
+		array[b] = array[end];
+		array[end] = temp;
 	}
+	write(1, array, 1);
+	c++;
 
 	return c;
 }
