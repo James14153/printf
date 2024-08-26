05a2b3495e5eae17a2b20236e49191544f0c2449
james14153 <jamesvincent903@gmail.com>
2024-08-26 00:50:42 -0700

    pointer address

--- p_print.c
+++ p_print.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /*
- * p_print - prints a pointer memory address in hexadecimal form
- * @arguments: list of arguments
+ * p_print - pprints the address of pointer in hexadecimal
+ * @arguments: takes a list of argumentst
  *
- * return the number of caharcters printed
+ * returns the number of characters printed
  *
  */
 
@@ -14,40 +14,39 @@ int p_print(va_list arguments)
 	char array[50];
 	unsigned long number;
 	char *hx = "0123456789abcdef";
-	int a = 2; 
-	int c = 0;
+	int a = 2;
+	int b, j, c = 0;
 
 	if (!ptr)
 	{
-		return (write(1, "(null)", 6));
+		return (write(1, "null", 6));
 	}
 
 	number = (unsigned long)ptr;
 	array[0] = '0';
 	array[1] = 'x';
-	
-	if (number == 0)
+
+	if (number == '0')
 	{
 		array[a++] = '0';
 	}
 	else
 	{
-
 		while (number > 0)
 		{
 			array[a++] = hx[number % 16];
 			number /= 16;
 		}
 
-	for (int b = 2; end = a - 1; b < end; b++; end--)
-	{
-		char temp = array[b];
-		array[b] = array[end];
-		array[end] = temp;
+		for (b = 2, j = b - 1; b < j; b++, j--)
+		{
+			char y = array[b];
+			array[b] = array[j];
+			array[j] = y;
+		}
 	}
-	c = write(1, array, a);
 
+	c = write(1, array, a);
 	return c;
 }
-
 
